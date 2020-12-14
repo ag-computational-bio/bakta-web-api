6fb50c4169771a92fe61c5a47eb95b55e90aa43e
Marius <mariusdieckmann90@gmail.com>
2020-12-15 00:02:48 +0100

    added proto swagger gen feature

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 protoc --proto_path=. --go-grpc_out=./proto/build --go_out=./proto/build --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/api/*.proto
+protoc -I . --grpc-gateway_out ./proto/build --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative proto/api/*
+
+protoc -I . --openapiv2_out ./proto/build --openapiv2_opt logtostderr=true proto/api/*
 
 mv proto/build/proto/api/*go go/api
+mv proto/build/proto/api/Jobs.swagger.json go/swaggerhandler
+
+go generate go/swaggerhandler/SwaggerGen.go
